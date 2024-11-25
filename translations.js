883b83d06270959c7ba5ed70d410433a34ec5d8e
Matheus Ricardo <matheusri1@hotmail.com>
2024-11-25 16:36:51 -0300

    Add files via upload

---
+++ translations.js
@@ -0,0 +1,40 @@
+const TRANSLATIONS = {
+    'pt-BR': {
+        score: 'PLACAR',
+        highScore: 'MELHOR PLACAR',
+        gameOver: 'FIM DE JOGO',
+        finalScore: 'Pontuação Final',
+        playAgain: 'JOGAR DE NOVO',
+        move: 'Mover',
+        shoot: 'Atirar',
+        alternativeControl: 'Controle Alternativo',
+        powerUps: {
+            doubleShot: 'Tiro Duplo',
+            speedBoost: 'Velocidade'
+        },
+        powerUpsLegend: {
+            title: 'Power-Ups:',
+            doubleShot: 'Tiro Duplo: 2 projéteis',
+            speedBoost: 'Turbo: +Velocidade'
+        }
+    },
+    'en-US': {
+        score: 'SCORE',
+        highScore: 'HIGH SCORE',
+        gameOver: 'GAME OVER',
+        finalScore: 'Final Score',
+        playAgain: 'PLAY AGAIN',
+        move: 'Move',
+        shoot: 'Shoot',
+        alternativeControl: 'Alternative Control',
+        powerUps: {
+            doubleShot: 'Double Shot',
+            speedBoost: 'Speed Boost'
+        },
+        powerUpsLegend: {
+            title: 'Power-Ups:',
+            doubleShot: 'Double Shot: 2 shots',
+            speedBoost: 'Speed Up: +Speed'
+        }
+    }
+}; 
